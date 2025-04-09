4eff30e8722686fc36524a0791c167542cbf1a51
Diego Neri <diego.holanda.neri@gmail.com>
2025-04-09 14:37:50 -0300

    Incluindo arquivos para criação de biblioteca

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
+from dotenv import load_dotenv
+import os
+
+# Carrega variáveis do arquivo .env
+load_dotenv()
 
 setup(
     name="dados-abertos-setor-eletrico",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        # Copia daqui do seu requirements.txt
+        "requests",
+        "pandas"
     ],
     author="Diego Neri",
-    author_email="seu-email@example.com",  # Troque para seu email real
+    author_email=os.getenv("AUTHOR_EMAIL"),
     description="Coleta e tratamento de dados públicos do setor elétrico brasileiro (ONS e ANEEL).",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
